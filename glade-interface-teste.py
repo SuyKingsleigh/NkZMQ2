ba66a3c77b9ab16263d2ccd2e70f18b5216a91fc
SuyKingsleigh <suymorielwiese@gmail.com>
2018-11-03 00:56:47 -0300

    Novo branch criado. >Corrigir bug de quando fecha a janela do terminal >Melhorar interface grafica.
    >cria uma classe apenas pra entrada de dados
    >essa classe cria uma janela para extrair dados
    >esses dados serao usados para adicionar/atualizar/remover redes do repositorio

--- glade-interface-teste.py
+++ glade-interface-teste.py
@@ -2,7 +2,7 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
-class Handler:
+class InterfaceHandler(Gtk.Window, Client):
     def __init__(self):
         # building a builder
         self.builder = Gtk.Builder()
@@ -14,14 +14,46 @@ class Handler:
         # get interface box
         self.interfaceBox = self.builder.get_object("interface-box")
 
-        self.mainWindow.show_all()
+        # get interface grid
+        self.interfaceGrid = self.builder.get_object('interface-grid')
+        self.menuBar = self.builder.get_object('menu-bar')
+
+
+        # file-chooser
+        self.fileChoser = self.builder.get_object('file-chooser')
 
         self.builder.connect_signals(self)
+        # Gtk.main()
 
-    def on_destroy(self):
+    def on_destroy(self, *args):
         Gtk.main_quit()
 
 
-a = Handler()
-Gtk.main()
+    def add_network_button(self):
+        # abrir uma janela, extrair author, preferences e description
+        # fechar a janela
+        # abrir o file-chooser
+        # abrir o arquivo
+        # carrega-lo em algum lugar
+        # envia-lo
+        # imprimir uma mensagem q deu bom
+        pass
+
+    def get_grid(self):
+        return self.interfaceGrid
+
+    def get_user_data(self):
+        # talvez seja melhor fazer isso com uma classe interna?
+        self.user_input = Gtk.Builder()
+        self.user_input.add_from_file('user_input.glade')
+
+        self.name_input= self.user_input.get_object('name_input')
+        self.author_input = self.user_input.get_object('author_input')
+        self.description_input = self.user_input.get_object('description_input')
+        self.preferences_input = self.user_input.get_object('preferences_input')
+
+
+
+    def runMain(self):
+        Gtk.main()
 
